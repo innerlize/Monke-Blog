f4a8d4250d2d68ae4e5dda9fed6e632233f2d390
Alan <tiltedveggie@outlook.kr>
2023-04-25 01:49:55 -0300

    Create actions handlers for forms

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,3 +1,45 @@
+import axios from 'axios';
+
 export function handleNavMenu(menu, setMenu) {
 	setMenu(!menu);
 }
+
+export const handleChange = (e, setValues) => {
+	setValues(prev => ({ ...prev, [e.target.name]: e.target.value }));
+};
+
+export const handleLogin = async (
+	e,
+	values,
+	setError,
+	navigate,
+	navigateTo
+) => {
+	e.preventDefault();
+
+	try {
+		await axios.post('auth/login', values);
+		navigate(navigateTo);
+	} catch (err) {
+		console.log(err);
+		setError(err);
+	}
+};
+
+export const handleRegister = async (
+	e,
+	values,
+	setError,
+	navigate,
+	navigateTo
+) => {
+	e.preventDefault();
+
+	try {
+		await axios.post('auth/register', values);
+		navigate(navigateTo);
+	} catch (err) {
+		console.log(err);
+		setError(err);
+	}
+};
