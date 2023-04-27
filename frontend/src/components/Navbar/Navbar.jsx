2122f7d7310c7981472e2abc8082d7c8b9b9698c
Alan <tiltedveggie@outlook.kr>
2023-04-27 15:05:51 -0300

    Use auth context and add new categories

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,32 +1,56 @@
-import React, { useState } from 'react';
+import React, { useContext } from 'react';
+import { AuthContext } from '../../contexts/authContext';
 import { Link } from 'react-router-dom';
-import { handleNavMenu } from '../../api/api';
-import styles from './Navbar.module.scss';
 
 const Navbar = () => {
-	const [showMenu, setShowMenu] = useState(false);
+	const { currentUser, logout } = useContext(AuthContext);
 
 	return (
-		<nav className={styles.nav}>
-			<div className={styles.logo}>
+		<nav>
+			<div>
 				<img src={require('../../assets/images/logo_txt-hzl.png')} />
 			</div>
 
-			<div onClick={() => handleNavMenu(showMenu, setShowMenu)}>Hamb</div>
-
-			<div className={styles.links_cont}>
+			<div>
 				<ul>
 					<li>
 						<Link to='/'>Home</Link>
 					</li>
+
+					<li>
+						<Link to='/?category=pets'>Pets</Link>
+					</li>
+
+					<li>
+						<Link to='/?category=wild%20animals'>Wild Animals</Link>
+					</li>
+
+					<li>
+						<Link to='/?category=animal%20care'>Animal Care</Link>
+					</li>
+
+					<li>
+						<Link to='/?category=training'>Training</Link>
+					</li>
+
+					<li>
+						<Link to='/?category=tips%20and%20advices'>Tips & Advices</Link>
+					</li>
+
 					<li>
 						<Link to='/about'>About</Link>
 					</li>
-					<li>
+
+					{currentUser?.username && <li> {currentUser.username} </li>}
+
+					{currentUser ? (
+						<Link onClick={() => logout()}>Logout</Link>
+					) : (
 						<Link to='/login'>Login</Link>
-					</li>
+					)}
+
 					<li>
-						<Link to='/login'>Post</Link>
+						<Link>Post</Link>
 					</li>
 				</ul>
 			</div>
