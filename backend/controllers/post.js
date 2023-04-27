a14f190d0f78f22d528c105b1d8122432976650d
Alan <tiltedveggie@outlook.kr>
2023-04-27 15:13:46 -0300

    Create routes and controllers for posts

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -1,3 +1,29 @@
+const db = require('../models/db.js');
+
+exports.getPosts = (req, res) => {
+	const sqlQuery = req.query.category
+		? 'SELECT * FROM posts WHERE category = ?'
+		: 'SELECT * FROM posts';
+
+	db.query(sqlQuery, [req.query.category], (err, data) => {
+		if (err) return res.json(err);
+
+		return res.status(200).json(data);
+	});
+};
+
+exports.getSinglePost = (req, res) => {
+	res.json('From controller ;3');
+};
+
 exports.addPost = (req, res) => {
+	res.json('From controller ;3');
+};
+
+exports.updatePost = (req, res) => {
+	res.json('From controller ;3');
+};
+
+exports.deletePost = (req, res) => {
 	res.json('From controller ;3');
 };
--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,4 +1,17 @@
 const express = require('express');
+const {
+	addPost,
+	getPosts,
+	getSinglePost,
+	updatePost,
+	deletePost
+} = require('../controllers/post.js');
 const router = express.Router();
+
+router.get('/', getPosts);
+router.get('/:id', getSinglePost);
+router.post('/', addPost);
+router.put('/:id', updatePost);
+router.delete('/:id', deletePost);
 
 module.exports = router;
