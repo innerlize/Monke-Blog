8e880f90437e9fefe477121f5371344632a4163c
Alan <tiltedveggie@outlook.kr>
2023-04-30 10:40:03 -0300

    Use Link component to go to new post and make images smaller for development progress

--- frontend/src/components/SideMenu/SidePost/SidePost.jsx
+++ frontend/src/components/SideMenu/SidePost/SidePost.jsx
@@ -3,14 +3,16 @@ import { Link } from 'react-router-dom';
 
 const SidePost = ({ post }) => {
 	return (
-		<div>
-			<Link>
-				<img src={post.img} />
-				<h4>{post.title}</h4>
-				<p>Posted by Erica</p>
-				<p>2 days ago</p>
-			</Link>
-		</div>
+		<Link to={`/post/${post.id}`}>
+			<div>
+				<div>
+					<img src={post.img} width={300} />
+					<h4>{post.title}</h4>
+					<p>Posted by Erica</p>
+					<p>2 days ago</p>
+				</div>
+			</div>
+		</Link>
 	);
 };
 
