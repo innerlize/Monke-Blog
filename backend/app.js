f1b1e4d9d0e7fddb39daff9f29322a95bc66db71
Alan <tiltedveggie@outlook.kr>
2023-04-30 12:59:19 -0300

    Import multer and configure route, usage and details and dist folder for files

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
+const multer = require('multer');
 const express = require('express');
 const authRoutes = require('./routes/auth.js');
 const userRoutes = require('./routes/users.js');
@@ -13,6 +14,28 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
+
+// --------------------------------------------------
+
+const storage = multer.diskStorage({
+	destination: (req, file, cb) => {
+		cb(null, '../frontend/public/uploads/');
+	},
+
+	filename: (req, file, cb) => {
+		cb(null, Date.now() + file.originalname);
+	}
+});
+
+const upload = multer({ storage });
+
+app.post('/api/upload', upload.single('file'), (req, res) => {
+	const file = req.file;
+
+	res.status(200).json(file.filename);
+});
+
+// --------------------------------------------------
 
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
