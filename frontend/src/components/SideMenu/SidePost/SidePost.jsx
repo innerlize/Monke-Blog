7ac1bd2d16e4ece5792db0eb6a74b632a8fa13cd
Alan <tiltedveggie@outlook.kr>
2023-05-04 11:43:53 -0300

    Import and use moment package and truncateText handler and modify post image path and scroll to top when click on related post

--- frontend/src/components/SideMenu/SidePost/SidePost.jsx
+++ frontend/src/components/SideMenu/SidePost/SidePost.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { truncateText } from '../../../api/api.js';
+import moment from 'moment';
 
 const SidePost = ({ post }) => {
 	return (
-		<Link to={`/post/${post.id}`}>
+		<Link
+			to={`/post/${post.id}`}
+			onClick={() => (document.documentElement.scrollTop = 0)}>
 			<div>
 				<div>
-					<img src={post.img} width={300} />
+					<img src={'../uploads/' + post.img} width={200} />
 					<h4>{post.title}</h4>
-					<p>Posted by Erica</p>
-					<p>2 days ago</p>
+					<p>{truncateText(post.description, 50)}</p>
+					<p>Posted {moment(post.date).fromNow()}</p>
 				</div>
 			</div>
 		</Link>
