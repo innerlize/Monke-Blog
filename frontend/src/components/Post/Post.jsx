f8bec01acd19079beee420437f41957fa9c84d98
Alan <tiltedveggie@outlook.kr>
2023-05-04 11:39:58 -0300

    Import and use moment package and truncateText handler and modify post image path

--- frontend/src/components/Post/Post.jsx
+++ frontend/src/components/Post/Post.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { truncateText } from '../../api/api.js';
+import moment from 'moment';
 
 const Post = ({ post }) => {
 	return (
 		<div>
 			<Link to={`/post/${post.id}`}>
 				<div>
-					<img src={post.img} width={300} />
+					<img src={'../uploads/' + post.img} width={300} />
 				</div>
 
 				<div>
 					<h3>{post.title}</h3>
-					<p>Posted by Lulu</p>
-					<p>2 days ago</p>
+					<p>{truncateText(post.description, 50)}</p>
+					<p>{moment(post.date).fromNow()}</p>
 				</div>
 			</Link>
 		</div>
