b1f335119a03ddb7e1c79347e59a6a9040300ad0
Alan <tiltedveggie@outlook.kr>
2023-05-06 15:47:20 -0300

    Add styles and some new features for navbar

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,58 +1,74 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { AuthContext } from '../../contexts/authContext';
 import { Link } from 'react-router-dom';
+import { Spin as Hamburger } from 'hamburger-react';
+import styles from './Navbar.module.scss';
 
 const Navbar = () => {
+	const [isActive, setIsActive] = useState(false);
 	const { currentUser, logout } = useContext(AuthContext);
 
 	return (
-		<nav>
-			<div>
+		<nav className={styles.nav}>
+			<Link className={styles.logo} to='/'>
 				<img src={require('../../assets/images/logo_txt-hzl.png')} />
-			</div>
+			</Link>
 
-			<div>
-				<ul>
-					<li>
+			<Hamburger toggle={setIsActive} toggled={isActive} />
+
+			<div
+				onClick={() => setIsActive(!isActive)}
+				className={isActive ? styles.links_cont_on : styles.links_cont_off}>
+				<div className={styles.sub_cont}>
+					<p>Pages</p>
+
+					<div>
 						<Link to='/'>Home</Link>
-					</li>
 
-					<li>
-						<Link to='/?category=pets'>Pets</Link>
-					</li>
-
-					<li>
-						<Link to='/?category=wild-animals'>Wild Animals</Link>
-					</li>
-
-					<li>
-						<Link to='/?category=animal-care'>Animal Care</Link>
-					</li>
-
-					<li>
-						<Link to='/?category=training'>Training</Link>
-					</li>
-
-					<li>
-						<Link to='/?category=tips-n-advices'>Tips & Advices</Link>
-					</li>
-
-					<li>
 						<Link to='/about'>About</Link>
-					</li>
 
-					{currentUser?.username && <li> {currentUser.username} </li>}
-
-					{currentUser ? (
-						<Link onClick={() => logout()}>Logout</Link>
-					) : (
-						<Link to='/login'>Login</Link>
-					)}
-
-					<li>
 						<Link to='/write'>Post</Link>
-					</li>
-				</ul>
+					</div>
+				</div>
+
+				<div className={styles.sub_cont}>
+					<p>Categories</p>
+
+					<div>
+						<Link to='/?category=pets'>Pets</Link>
+
+						<Link to='/?category=wild-animals'>Wild Animals</Link>
+
+						<Link to='/?category=animal-care'>Animal Care</Link>
+
+						<Link to='/?category=training'>Training</Link>
+
+						<Link to='/?category=tips-n-advices'>Tips & Advices</Link>
+					</div>
+				</div>
+
+				<div className={styles.sub_cont}>
+					<p>Account</p>
+
+					<div>
+						{!currentUser && (
+							<>
+								<Link to='/login'>Login</Link>
+								<Link to='/register'>Register</Link>
+							</>
+						)}
+
+						{currentUser && (
+							<>
+								<span className={styles.username}>
+									Signed in as: <span>{currentUser.username}</span>
+								</span>
+
+								<Link onClick={() => logout()}>Logout</Link>
+							</>
+						)}
+					</div>
+				</div>
 			</div>
 		</nav>
 	);
