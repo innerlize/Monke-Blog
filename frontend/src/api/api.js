812e94f669ee97615adc1ce2938e8d691731b33c
Alan <tiltedveggie@outlook.kr>
2023-05-06 15:45:58 -0300

    Add function for parse string to HTML and import and use it in truncateText function

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -6,17 +6,24 @@ export const handleChange = (e, setValues) => {
 };
 
 export const truncateText = (str, length, ending) => {
+	let parsedString = textToHTML(str);
+
 	if (length == null) {
 		length = 100;
 	}
 	if (ending == null) {
 		ending = '...';
 	}
-	if (str.length > length) {
-		return str.substring(0, length - ending.length) + ending;
+	if (parsedString.length > length) {
+		return parsedString.substring(0, length - ending.length) + ending;
 	} else {
-		return str;
+		return parsedString;
 	}
+};
+
+export const textToHTML = text => {
+	const doc = new DOMParser().parseFromString(text, 'text/html');
+	return doc.body.textContent;
 };
 
 export const handleLogin = async (
