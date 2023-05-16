33227a87d17b2f81d727fdb73ef73268bc6df041
Alan <tiltedveggie@outlook.kr>
2023-05-16 01:49:13 -0300

    Delete trailing slash at origin URL

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ const app = express();
 
 // --------------------------------------------------
 
-app.use(cors({ origin: 'https://monke-blog.vercel.app/', credentials: true }));
+app.use(cors({ origin: 'https://monke-blog.vercel.app', credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
