c55fccf98893319d063681a3bf0718a1ae4ba1d7
Alan <tiltedveggie@outlook.kr>
2023-05-16 00:53:53 -0300

    Try to fix CORS error using vercel.json

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const multer = require('multer');
 const express = require('express');
@@ -12,6 +13,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors());
 
 // --------------------------------------------------
 
